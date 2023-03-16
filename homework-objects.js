3da73490d943546cb67b767acd9dee5233e5d1a9
Oleksandr <pyvko.a.g@gmail.com>
2023-03-16 11:17:19 +0200

    finished 3rd part

--- homework-objects.js
+++ homework-objects.js
@@ -34,21 +34,47 @@ console.log(periodOfTime('02 Aug 1985', '03 Aug 1985', 'seconds'));
 console.log(periodOfTime('31 Jan 2022', '03 Feb 2021', 'days'));
 console.log(periodOfTime());
 
-
 // 2
 
 const priceData = {
-Apples: '23.4',
-BANANAS: '48',
-oRAngGEs: '48.7584',
- };
+  Apples: '23.4',
+  BANANAS: '48',
+  oRAngGEs: '48.7584',
+};
 function optimizer(data) {
-  const formattedObj = { };
+  const formattedObj = {};
   for (const [key, value] of Object.entries(data)) {
     formattedObj[key.toLowerCase()] = Number(value).toFixed(2);
   }
   return formattedObj;
-
-};
+}
 let updatedPriceData = optimizer(priceData);
-console.log(updatedPriceData)    // {apples: '23.40', bananas: '48.00', oranges: '48.76'} 
+console.log(updatedPriceData); // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
+
+// 3  recursive
+
+function recursiveOddSumTo(number) {
+  if (number <= 0) {
+    return 0;
+  }
+  if (number % 2 === 0) {
+    number--;
+  }
+  return number + recursiveOddSumTo(number - 2);
+}
+
+console.log(recursiveOddSumTo(1)); // 1
+console.log(recursiveOddSumTo(10)); // 25
+
+// 3.1 iterative
+
+function iterativeOddSumTo(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i += 2) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(iterativeOddSumTo(1)); // 1
+console.log(iterativeOddSumTo(10)); // 25
