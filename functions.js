7c40c763b4294afad922ebeda5c0bbde7b50ee92
Oleksandr <pyvko.a.g@gmail.com>
2023-03-25 14:17:11 +0200

    finished 3rd

--- functions.js
+++ functions.js
@@ -23,3 +23,46 @@ function multiply(a) {
 console.log(multiply(5)(5)); // 25*
 console.log(multiply(2)(-2)); // -4*
 console.log(multiply(4)(3)); // 12*
+
+// 3
+
+const movies = [
+  {
+    movieName: 'The Thing',
+    releaseYear: 1982,
+    directedBy: 'Carpenter',
+    runningTimeInMinutes: 109,
+  },
+  {
+    movieName: 'Aliens',
+    releaseYear: 1986,
+    directedBy: 'Cameron',
+    runningTimeInMinutes: 137,
+  },
+  {
+    movieName: 'Men in Black',
+    releaseYear: 1997,
+    directedBy: 'Sonnenfeld',
+    runningTimeInMinutes: 98,
+  },
+  {
+    movieName: 'Predator',
+    releaseYear: 1987,
+    directedBy: 'McTiernan',
+    runningTimeInMinutes: 107,
+  },
+];
+
+console.log(movies.sort(byProperty('releaseYear', '>'))); 
+console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); 
+console.log(movies.sort(byProperty('movieName', '<'))); 
+
+function byProperty(property, direction) {
+  return function (a, b) {
+    if (direction === '>') {
+      return a[property] > b[property] ? -1 : 1;
+    } else if (direction === '<') {
+      return a[property] < b[property] ? -1 : 1;
+    }
+  };
+}
