5557e31178c047f2cfaadac8689f18d57a02a84f
Oleksandr <pyvko.a.g@gmail.com>
2023-05-18 17:12:35 +0300

    reworked iterator func

--- iterators and generators/index.js
+++ iterators and generators/index.js
@@ -1,29 +1,24 @@
 //Iterator
-const createiterator = () => {
-  let number = 1;
+const createIterator = () => {
+  let number = 0;
   return {
     next: () => {
+      number++;
       if (number > 100) {
         return { done: true };
-      } else if (number % 15 === 0) {
-        number++;
+      } if (number % 15 === 0) {
         return 'FizzBuzz';
-      } else if (number % 3 === 0) {
-        number++;
+      } if (number % 3 === 0) {
         return 'Fizz';
-      } else if (number % 5 === 0) {
-        number++;
+      } if (number % 5 === 0) {
         return 'Buzz';
-      } else {
-        number++;
-        const result = { value: number, done: false };
-        return result.value;
       }
+      return { value: number, done: false }
     },
   };
-};
+}
 
-const iterator = createiterator();
+const iterator = createIterator();
 
 let counter = 0;
 while (counter < 100) {
@@ -33,37 +28,37 @@ while (counter < 100) {
 
 //Generator function
 
-function* FizzBuzz() {
-  for (let i = 1; i < 100; i++) {
-    if (i % 15 === 0) {
-      yield 'FizzBuzz';
-    } else if (i % 3 === 0) {
-      yield 'Fizz';
-    } else if (i % 5 === 0) {
-      yield 'Buzz';
-    } else {
-      yield i;
-    }
-  }
-}
+// function* FizzBuzz() {
+//   for (let i = 1; i < 100; i++) {
+//     if (i % 15 === 0) {
+//       yield 'FizzBuzz';
+//     } else if (i % 3 === 0) {
+//       yield 'Fizz';
+//     } else if (i % 5 === 0) {
+//       yield 'Buzz';
+//     } else {
+//       yield i;
+//     }
+//   }
+// }
 
-const generator = FizzBuzz();
+// const generator = FizzBuzz();
 
-let result = generator.next();
-while (!result.done) {
-  console.log(result.value);
-  result = generator.next();
-}
+// let result = generator.next();
+// while (!result.done) {
+//   console.log(result.value);
+//   result = generator.next();
+// }
 
-// Random number generator
-function* random(max, quantity) {
-  for (let i = 0; i < quantity; i++) {
-    yield Math.floor(Math.random() * max);
-  }
-}
+// // Random number generator
+// function* random(max, quantity) {
+//   for (let i = 0; i < quantity; i++) {
+//     yield Math.floor(Math.random() * max);
+//   }
+// }
 
-const randomNumbers = random(100, 5);
+// const randomNumbers = random(100, 5);
 
-for (let numbers of randomNumbers) {
-  console.log(numbers);
-}
+// for (let numbers of randomNumbers) {
+//   console.log(numbers);
+// }
