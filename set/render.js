2264b8a1ef6b0bcc3993d73c1060c70deb87d68c
Oleksandr <pyvko.a.g@gmail.com>
2023-05-23 16:47:12 +0300

    edited

--- set/render.js
+++ set/render.js
@@ -3,14 +3,9 @@ import { getArray, getSet } from './dataManager.js';
 const renderArray = (arrayElement) => {
   const array = getArray();
 
-  arrayElement.innerHTML = '';
-  array.forEach((value) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = value + ',';
-    arrayElement.appendChild(listItem);
-    console.log(value);
-  });
-};
+  arrayElement.innerHTML = array.join(',');
+  };
+
 
 const renderSet = (setElement) => {
   const set = getSet();
